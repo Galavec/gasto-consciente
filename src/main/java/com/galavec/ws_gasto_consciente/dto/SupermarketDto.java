23fe1dd12d805381242b1a48e5722367917a7154
Hector Galarza <galavec@hotmail.com>
2024-08-11 22:54:12 -0500

    fix: se corrigen los message de los campos del SupermarketDto

--- src/main/java/com/galavec/ws_gasto_consciente/dto/SupermarketDto.java
+++ src/main/java/com/galavec/ws_gasto_consciente/dto/SupermarketDto.java
@@ -16,15 +16,15 @@ import java.time.LocalDateTime;
 @AllArgsConstructor
 @NoArgsConstructor
 public class SupermarketDto {
-    @NotNull(message = "El código del supermercado no puede ser nulo.")
+    @NotNull(message = "El nombre del supermercado no puede ser nulo.")
     @Size(min = 1, max = 255, message = "El nombre del supermercado debe tener entre 1 y 255 caracteres.")
     private String supermarketName;
 
-    @Size(min = 0, max = 500, message = "El nombre del supermercado debe tener entre 1 y 255 caracteres.")
+    @Size(min = 0, max = 500, message = "La descripción para el supermercado debe tener entre 1 y 500 caracteres.")
     private String supermarketDescription;
 
     @NotNull(message = "El código del usuario de creación no puede ser nulo.")
-    @Size(min = 1, max = 50, message = "El código de usuario debe tener entre 1 y 50 caracteres.")
+    @Size(min = 1, max = 50, message = "El código del usuario de creación debe tener entre 1 y 50 caracteres.")
     private String userCreation;
 
     @NotNull(message = "La fecha de creación no puede ser nulo.")
